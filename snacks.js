58a6995f1c0224e251b844649073c24098cfad59
Marco Bacci <marcobacci94@gmail.com>
2025-12-12 15:45:36 +0100

    Feat: snack 3 completed

--- snacks.js
+++ snacks.js
@@ -1,9 +1,25 @@
 // snack 1
 function getInitials(string) {
   const [name, surname] = string.split(" ");
-  return `${name.charAt(0)}${surname.charAt(0)}`
+  return `${name.charAt(0)}${surname.charAt(0)}`;
+}
+
+// snack 2
+function createSlug(string) {
+  return string.toLowerCase();
+}
+
+// snack 3
+
+function average(numbers) {
+  const sum = numbers.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+  return sum / numbers.length;
 }
 
 module.exports = {
   getInitials,
+  createSlug,
+  average,
 };
--- snacks.test.js
+++ snacks.test.js
@@ -1,6 +1,17 @@
-const { getInitials } = require("./snacks");
+const { getInitials, createSlug, average } = require("./snacks");
 
 // snack 1
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
   expect(getInitials("Marco Bacci")).toBe("MB");
+});
+
+// snack 2
+test("La funzione createSlug restituisce una stringa in lowercase", () => {
+  expect(createSlug("HELLO WORLD")).toBe("hello world");
+});
+
+// snack 3
+test("La funzione average calcola la media aritmetica di un array di numeri", () => {
+  expect(average([6, 10, 8])).toBe(8);
+  expect(average([20, 10, 30])).toBe(20);
 });
